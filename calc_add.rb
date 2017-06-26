176db7bf67eeaabe6959a2bc81d9b45b590220fd
Ty-Zeta <tyofzeta@gmail.com>
2017-06-26 11:25:42 -0400

    updated addition with gits.chomp

--- calc_add.rb
+++ calc_add.rb
@@ -1,4 +1,6 @@
-def add(a, b)
-  return a + b
-end
-p add(10, 5)
+p "Enter first number to add"
+n1 = gets.chomp.to_i
+p "Enter second number to add"
+n2 = gets.chomp.to_i
+n3 = n1 + n2
+puts "The answer to #{n1} + #{n2} = #{n3}."
