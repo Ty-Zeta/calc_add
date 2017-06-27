206110a378cd9a39a1e29ecb5df2c3e8311331da
Ty-Zeta <tyofzeta@gmail.com>
2017-06-27 10:16:49 -0400

    updated add within function

--- example.rb
+++ example.rb
@@ -1,8 +1,7 @@
 p "Enter first number to add"
-def add(n1,n2)
-  puts  "#{n1} + #{n2} = #{n1 + n2}"
-end
+def 
 n1 = gets.chomp.to_i
 p "Enter second number to add"
 n2 = gets.chomp.to_i
-add(n1, n2)
+n3 = n1 + n2
+puts "The answer to #{n1} + #{n2} = #{n3}."
